84907dae7d5f2e39ea4e049b603e1a407e0c2832
Daniel Escareno <daniel.esc@outlook.com>
2024-03-18 02:36:30 -0600

    add git branches telscope buildtin keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -336,7 +336,6 @@ require("lazy").setup({
 			-- This opens a window that shows you all of the keymaps for the current
 			-- telescope picker. This is really useful to discover what Telescope can
 			-- do as well as how to actually do it!
-
 			-- [[ Configure Telescope ]]
 			-- See `:help telescope` and `:help telescope.setup()`
 			require("telescope").setup({
@@ -372,7 +371,7 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
+			vim.keymap.set("n", "<leader>tgb", builtin.git_branches, { desc = "[T]elescope [G]it [B]ranches" })
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
 				-- You can pass additional configuration to telescope to change theme, layout, etc.
